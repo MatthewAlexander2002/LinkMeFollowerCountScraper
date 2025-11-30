1aaf2085d70d3feb50dfee732846befa7aeb5688
MatthewAlexander2002 <matthewalexander009@gmail.com>
2025-12-01 08:34:28 +1100

    I can now extract the value, next need to create the embedded page

--- scraper.py
+++ scraper.py
@@ -1,7 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://realpython.github.io/fake-jobs/"
+URL = "https://link.me/k1ngkyrg1os"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+
+profile = soup.find(id="profileContainer")
+
+total_follower_count = profile.find("span", class_="font-semibold mr-1")
+
+# print(total_follower_count)
+
+# why does this work?
+total_follower_count_clean = ""
+for word in total_follower_count:
+    total_follower_count_clean
+    
