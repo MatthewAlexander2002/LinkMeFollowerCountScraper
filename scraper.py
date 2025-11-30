077f8134565718ae87f4c620f27287e12fa56883
MatthewAlexander2002 <126222510+MatthewAlexander2002@users.noreply.github.com>
2025-12-01 09:45:55 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def main_page():
-    return get_total_follower_count()
-
 
 def get_total_follower_count():
     URL = "https://link.me/k1ngkyrg1os"
@@ -16,16 +8,10 @@ def get_total_follower_count():
     soup = BeautifulSoup(page.content, "html.parser")
 
     profile = soup.find(id="profileContainer")
-
     total_follower_count = profile.find("span", class_="font-semibold mr-1")
 
-    # print(total_follower_count)
-
-    # why does this work?
-    # total_follower_count_clean = ""
     for word in total_follower_count:
-        return word
-    
+        return word.strip()
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    print(get_total_follower_count())
